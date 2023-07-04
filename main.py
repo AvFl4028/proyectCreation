597b259d386fc79ec9b317d78ad0fd1f10cc5e07
AvFl4028 <aviel4028@gmail.com>
2023-07-04 17:17:32 -0600

    first

---
+++ main.py
@@ -0,0 +1,93 @@
+import subprocess
+import os
+
+
+def create_directory(path):
+    if not os.path.exists(path):
+        # Crear el directorio
+        os.makedirs(path)
+        print("Directorio creado exitosamente.")
+    else:
+        print("El directorio ya existe.")
+
+
+def command_maker_react(directory):
+    print("Esto puede tardar unos minutos (depende de su internet)")
+    output = subprocess.check_output(f"npx create-react-app {directory}", shell=True)
+    print(output.decode())
+
+
+def command_maker_js(directory):
+    html = open(f"{directory}/index.html", "w")
+    js = open(f"{directory}/main.js", "w")
+    css = open(f"{directory}/style.css", "w")
+
+    html.write('<!DOCTYPE html>\n'
+               '<html lang="en">\n'
+               '  <head>\n'
+               '    <meta charset="utf-8" />\n'
+               '    <title></title>\n'
+               '  <meta name="viewport" content="initial-scale=1, width=device-width" />\n'
+               '  </head>\n'
+               '  <body>\n'
+               '  </body>\n'
+               '</html>\n')
+
+    js.write("alert('test')")
+
+    css.write("* {\n\n}")
+
+    html.close()
+    js.close()
+    css.close()
+
+    print("Archivos generados exitosamente")
+
+
+def command_maker_arduino():
+    print("Ruta: ")
+    path = input()
+    print("Nombre del proyecto")
+    project_name = input()
+
+    path_full = path + "/" + project_name
+
+    create_directory(path_full)
+
+    ino = open(f"{path_full}/{project_name}.ino", "w")
+    ino.write("void setup(){\n\n}\n")
+    ino.write("\nvoid loop(){\n\n}\n")
+    ino.close()
+    print("Archivos generados exitosamente")
+
+
+def select_project_for_create():
+    global directory_path
+    print("Selecciona una opción")
+    print("1- React Vanilla (default)     2- JS Vanilla   3- Arduino")
+    value = input()
+
+    if value == "":
+        value = "1"
+
+    if value != "3":
+        print("Ruta (con carpeta sin crear o vacia): ")
+        directory_path = input()
+
+    if value == "1":
+        create_directory(directory_path)
+        command_maker_react(directory_path)
+        return
+
+    if value == "2":
+        create_directory(directory_path)
+        command_maker_js(directory_path)
+        return
+
+    if value == "3":
+        command_maker_arduino()
+        return
+
+
+if __name__ == "__main__":
+    select_project_for_create()
